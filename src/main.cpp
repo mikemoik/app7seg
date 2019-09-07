75fbcd6b65e130199adb2a98e1d0137718157160
Mike Moik <mikemoik@gmail.com>
2019-09-07 10:09:19 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <Arduino.h>
 #include <TemplClass.h>
 
+/**
+*
+*
+* pio flash command:
+* $ pio lib update; pio run -e nodemcuv2 -t upload
+**/
+
 Templ::TemplClass _templ;
 
 void setup() {
