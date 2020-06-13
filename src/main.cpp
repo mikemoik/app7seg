58a92a17acee64d0259d9f8d9eac08067a5f755d
Mike Moik <mikemoik@gmail.com>
2020-06-14 00:46:04 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@
 #define LED_NUM 4
 #define LED_MODE (NEO_GRB + NEO_KHZ800)
 #define LED_NUM_PER_SEGMENT 1
-#define CHAR_NUM 4
+#define CHAR_NUM 1
 
 NeoPixel::String7Seg _7seg(CHAR_NUM, LED_NUM_PER_SEGMENT, LED_NUM, LED_PIN, LED_MODE);
 
@@ -22,10 +22,13 @@ void setup() {
 }
 
 void loop() {
-  _7seg._strip->setPixelColor(0, 0xFFU, 0x0U , 0x0U , 0x0U );
-  _7seg._strip->setPixelColor(1, 0x0U , 0xFFU, 0x0U , 0x0U );
-  _7seg._strip->setPixelColor(2, 0x0U , 0x0U , 0xFFU, 0x0U );
-  _7seg._strip->setPixelColor(3, 0x0U , 0x0U , 0x0U , 0xFFU);
+  _7seg.render();
+  
+  // DEBUG
+  //_7seg._strip->setPixelColor(0, 0xFFU, 0x0U , 0x0U , 0x0U );
+  //_7seg._strip->setPixelColor(1, 0x0U , 0xFFU, 0x0U , 0x0U );
+  //_7seg._strip->setPixelColor(2, 0x0U , 0x0U , 0xFFU, 0x0U );
+  //_7seg._strip->setPixelColor(3, 0x0U , 0x0U , 0x0U , 0xFFU);
 
   delay(1000);
 }
